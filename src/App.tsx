260f1a7aa00986976d2c8f954293d8983c586055
Ana Sena <luiza1fofa@gmail.com>
2024-12-15 12:00:38 -0300

    Configurando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import './App.css';
+import Teste from './Teste';
 
 function App() {
-  return <>
-  <h1>Hello React!</h1>
-  <h2>Testando erros</h2>
-  </>
+    return (
+        <>
+            <h1>Hello React!</h1>
+            <h2>Testando erros</h2>
+            <Teste />
+        </>
+    );
 }
 
 export default App;
